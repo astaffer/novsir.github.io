c2e089ad1aba2a3b55790e53314ec09958cfdba3
Астафьев Павел Михайлович <astpm@volhovez.local>
2015-12-22 12:49:44 +0300

    add more

--- js/controllers/ProductController.js
+++ js/controllers/ProductController.js
@@ -1,6 +1,14 @@
 app.controller('ProductController',
-    function ProductController($scope,dataService,$routeParams){
+    function ProductController($scope,dataService,productService,$routeParams){
+    
       var promiseObj=dataService.getProducts();
-        promiseObj.then(function(value) { $scope.detail=value.items[$routeParams.id]; });
+        promiseObj.then(function(value) {
+         $scope.detail =value.items[$routeParams.id];
+          var nav = $scope.detail.navigate;
+          var promObj=productService.getItems(nav);
+          promObj.then(function(value) { $scope.items=value; });
+          });
+ 	 
+    
     }
 )
---
+++ js/services/productService.js
@@ -0,0 +1,17 @@
+app.factory('productService', function($http, $q){
+    return{
+        getItems: function($param){
+            var deferred = $q.defer();
+            var $url = '\js\\json\\products\\'+$param+'.json'
+            $http({method: 'GET', url: $url}).
+             success(function(data, status, headers, config) {
+                deferred.resolve(data.products);
+            }).
+            error(function(data, status, headers, config) {
+                deferred.reject(status);
+            });
+             
+            return deferred.promise;
+        }
+    }
+})

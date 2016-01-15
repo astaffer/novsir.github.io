d1ae4127b5eea2353fd6a939501489167fb5d643
Астафьев Павел Михайлович <astpm@volhovez.local>
2016-01-15 13:03:51 +0300

    rem emal

--- js/mainApp.js
+++ js/mainApp.js
@@ -21,26 +21,7 @@ var main = function(){
 			$('#main_menu li').removeClass('active');
 			$(this).toggleClass('active');
 	});
-document.getElementById('myElement').onclick = function() {
-    		var xmlhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new 	ActiveXObject("Microsoft.XMLHTTP");
-	xmlhttp.open('POST', 'https://mandrillapp.com/api/1.0/messages/send.json');
-	xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-	xmlhttp.onreadystatechange = function() {
-	    if (xmlhttp.readyState == 4) {
-	        if(xmlhttp.status == 200) alert('Mail sended!')
-	        else if(xmlhttp.status == 500) alert('Check apikey')
-	        else alert('Request error');
-	    }
-	}
-	xmlhttp.send(JSON.stringify({'key': 'ES1fqDzSBDEpbOw1Kph4Pw',
-	   'message': {
-	       'from_email': 'astaffer@mail.ru',
-	       'to': [{'email': 'support@novsir.ru', 'type': 'to'}],
-	       'autotext': 'true',
-	       'subject': 'Yeah!',
-	       'html': '<h1>Its work!</h1>'
-	    }}));
-	}
+
 };
 //alert(window.location.href);
 //$(document).ready(main);

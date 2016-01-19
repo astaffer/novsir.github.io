90784fb9903f90c7dbb1bc030c7ff6f25944f8ff
Астафьев Павел Михайлович <astpm@volhovez.local>
2016-01-19 08:50:17 +0300

    add email sender

--- js/controllers/ContactsController.js
+++ js/controllers/ContactsController.js
@@ -1,6 +1,35 @@
 app.controller('ContactsController',
-    function ContactsController($scope,dataService){
+    function ContactsController($scope,$http,dataService){
       var promiseObj=dataService.getContacts();
         promiseObj.then(function(value) { $scope.contacts=value; });
+         $scope.save = function (answer, answerForm){
+            if(answerForm.$valid){
+					var param = JSON.stringify({'key': 'ES1fqDzSBDEpbOw1Kph4Pw',
+				   'message': {
+					   'from_email': 'website@novsir.ru',
+					   'to': [{'email': 'astaffer@mail.ru', 'type': 'to'}],
+					   'autotext': 'true',
+					   'subject': 'Сообщение с сайта novsir',
+					   'html': '<h1>Новое сообщение</h1>'
+							+'<b>Обращение:</b>'+answer.author
+							+'<br><b>E-mail адрес:</b>'+answer.email
+							+'<br><b>Текст обращения:</b>'+'<p>'+answer.text+'</p>'
+					}});
+					var url = 'https://mandrillapp.com/api/1.0/messages/send.json';
+		$http.post(url,param)
+				.success(function(data, status, headers, config) {
+				// this callback will be called asynchronously
+				// when the response is available
+				console.log(data);
+				// действия по сохранению
+                alert(answer.author + ", Ваше сообщение отправлено");
+			  }).
+			  error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+			  });
+                
+            }
+        };
     }
 )

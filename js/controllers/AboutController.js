8a95c11b08c164be1ec211fb285f26f0018a8f62
Астафьев Павел Михайлович <astpm@volhovez.local>
2016-05-26 15:28:48 +0300

    add actions

--- js/controllers/AboutController.js
+++ js/controllers/AboutController.js
@@ -4,5 +4,7 @@ app.controller('AboutController',
         promiseObj.then(function(value) { $scope.about=value; });
       var contObj =dataService.getContacts();
       contObj.then(function(value) { $scope.contacts=value; });
+	var actionsObj =dataService.getActions();
+      actionsObj.then(function(value) { $scope.actions=value; });
     }
 )
